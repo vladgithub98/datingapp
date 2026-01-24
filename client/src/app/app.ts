1e03ed7bd935c0e67a74e832ba4d53e80edccd4e
vladgithub98 <rvlaad98@gmail.com>
2026-01-24 17:44:41 +0200

    adding routes

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -3,17 +3,18 @@ import { Component, inject, OnInit, signal } from '@angular/core';
 import { lastValueFrom } from 'rxjs';
 import { Nav } from "../layout/nav/nav";
 import { AccountService } from '../core/services/account-service';
-import { Home } from "../features/home/home";
 import { User } from '../types/user';
+import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [Nav, Home],
+  imports: [Nav, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App implements OnInit {
   private accountService = inject(AccountService);
+  protected router = inject(Router);
   private http = inject(HttpClient);
   protected  title = 'Dating app';
   protected members = signal<User[]>([]);
--- client/src/layout/nav/nav.ts
+++ client/src/layout/nav/nav.ts
@@ -1,21 +1,23 @@
 import { Component, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../../core/services/account-service';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
-  imports: [FormsModule],
+  imports: [FormsModule, RouterLink, RouterLinkActive],
   templateUrl: './nav.html',
   styleUrl: './nav.css'
 })
 export class Nav {
   protected accountService = inject(AccountService)
   protected creds: any = {}
+  private router = inject(Router);
 
   login() {
     this.accountService.login(this.creds).subscribe({
       next: result => {
-        console.log(result);
+        this.router.navigateByUrl('/members');
         this.creds = {};
       },
       error: error => alert(error.message)
@@ -24,5 +26,6 @@ export class Nav {
 
   logout(){
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 }
