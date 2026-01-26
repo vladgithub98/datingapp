62ad0c3c13ea5250fa1c28b55b696f0cc017fa54
vladgithub98 <rvlaad98@gmail.com>
2026-01-26 15:53:27 +0200

    add Guards

--- client/src/layout/nav/nav.ts
+++ client/src/layout/nav/nav.ts
@@ -2,6 +2,7 @@ import { Component, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../../core/services/account-service';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { ToastService } from '../../core/services/toast-service';
 
 @Component({
   selector: 'app-nav',
@@ -13,14 +14,19 @@ export class Nav {
   protected accountService = inject(AccountService)
   protected creds: any = {}
   private router = inject(Router);
+  private toast = inject(ToastService);
 
   login() {
     this.accountService.login(this.creds).subscribe({
-      next: result => {
-        this.router.navigateByUrl('/members');
+      next: () => {
+        this.router.navigateByUrl('/members'); 
+        this.toast.success('Logged in succesfully');
         this.creds = {};
       },
-      error: error => alert(error.message)
+      error: error => {
+        console.log(error);
+        this.toast.error(error.error);
+      }
     })
   }
 
