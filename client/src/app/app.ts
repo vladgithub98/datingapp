4c15f036230e57ff22c5c2e010946e89f4165c46
vladgithub98 <rvlaad98@gmail.com>
2026-01-26 16:08:51 +0200

    End of section 6

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,9 +1,5 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, inject, OnInit, signal } from '@angular/core';
-import { lastValueFrom } from 'rxjs';
+import { Component, inject } from '@angular/core';
 import { Nav } from "../layout/nav/nav";
-import { AccountService } from '../core/services/account-service';
-import { User } from '../types/user';
 import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
@@ -12,32 +8,6 @@ import { Router, RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App implements OnInit {
-  private accountService = inject(AccountService);
+export class App {
   protected router = inject(Router);
-  private http = inject(HttpClient);
-  protected  title = 'Dating app';
-  protected members = signal<User[]>([]);
-
-  async ngOnInit() {
-    this.members.set(await this.getMembers());
-    this.setCurrentUser();
-  }
-
-  setCurrentUser() {
-    const userString = localStorage.getItem('user');
-    if(!userString) return;
-    const user = JSON.parse(userString);
-    this.accountService.currentUser.set(user);
-  }
-
-  async getMembers() {
-    try {
-      return lastValueFrom(this.http.get<User[]>('https://localhost:5001/api/members'));
-    }
-    catch (error) {
-      console.log(error);
-      throw error;
-    }
-  }
 }
